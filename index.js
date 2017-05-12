aa43bdc7c4d1c79ca0e284bee8404ceb36f92783
Michael Ledin <m.ledin@appheads.ru>
2017-05-12 22:46:47 +0300

    .editorconfig

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 module.exports = function (options) {
-    var gulp = require('gulp');
+  var gulp = require('gulp');
 
-    options = options || {};
-    var repeatOnEnter = options.repeatOnEnter || false;
-    var taskName = options.taskName || 'prompt';
+  options = options || {};
+  var repeatOnEnter = options.repeatOnEnter || false;
+  var taskName = options.taskName || 'prompt';
 
-    var lastPromptedTask;
+  var lastPromptedTask;
 
-    gulp.task(taskName, function (cb) {
-        var promptedTask;
+  gulp.task(taskName, function (cb) {
+    var promptedTask;
 
-        function startPrompt() {
-            promptedTask = undefined;
-            gulp.start(taskName);
+    function startPrompt() {
+      promptedTask = undefined;
+      gulp.start(taskName);
+    }
+
+    function onTaskEnd(event) {
+      setTimeout(function () {
+        if (event && promptedTask && event.task === promptedTask) {
+          lastPromptedTask = promptedTask;
+          gulp.removeListener('task_stop', onTaskEnd);
+          gulp.removeListener('task_err', onTaskEnd);
+          gulp.removeListener('task_not_found', onTaskEnd);
+          startPrompt();
         }
+      });
+    }
 
-        function onTaskEnd(event) {
-            setTimeout(function () {
-                if (event && promptedTask && event.task === promptedTask) {
-                    lastPromptedTask = promptedTask;
-                    gulp.removeListener('task_stop', onTaskEnd);
-                    gulp.removeListener('task_err', onTaskEnd);
-                    gulp.removeListener('task_not_found', onTaskEnd);
-                    startPrompt();
-                }
-            });
-        }
-
-        gulp.on('task_stop', onTaskEnd);
-        gulp.on('task_err', onTaskEnd);
-        gulp.removeAllListeners('task_not_found');
-        gulp.on('task_not_found', function (err) {
-            console.log('Task \'' + err.task + '\' is not in your gulpfile');
-            onTaskEnd(err);
-        });
-
-        var inquirer = require('inquirer');
-        inquirer.prompt([{ type: 'input', name: 'task', 'message': 'Enter gulp task name:' }])
-            .then(function (answers) {
-                promptedTask = answers.task;
-                if (repeatOnEnter) {
-                    promptedTask = promptedTask || lastPromptedTask;
-                }
-                promptedTask = promptedTask || taskName;
-                cb();
-                gulp.start(promptedTask);
-            });
+    gulp.on('task_stop', onTaskEnd);
+    gulp.on('task_err', onTaskEnd);
+    gulp.removeAllListeners('task_not_found');
+    gulp.on('task_not_found', function (err) {
+      console.log('Task \'' + err.task + '\' is not in your gulpfile');
+      onTaskEnd(err);
     });
+
+    var inquirer = require('inquirer');
+    inquirer.prompt([{ type: 'input', name: 'task', 'message': 'Enter gulp task name:' }])
+      .then(function (answers) {
+        promptedTask = answers.task;
+        if (repeatOnEnter) {
+          promptedTask = promptedTask || lastPromptedTask;
+        }
+        promptedTask = promptedTask || taskName;
+        cb();
+        gulp.start(promptedTask);
+      });
+  });
 };
